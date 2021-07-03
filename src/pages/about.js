c8b055bdbcc4614cab1229491673c035649108c3
Mohammad Afaj Ansari <afajansari@gmail.com>
2021-07-04 00:43:10 +0530

    First Commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+// import * from 'react'
+
+import React from 'react'
+
+function about() {
+    return (
+        <div>
+            <h1>THis is about page</h1>
+        </div>
+    )
+}
+
+export default about
+
